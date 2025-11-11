9b18425082a34618dfc86f6424fbb6b99b6fdbd3
jabarmagdiel <jabarmagdiel@gmail.com>
2025-11-11 08:06:22 -0400

    Implement NotificationConsumer for WebSocket notifications

--- sales/consumers.py
+++ sales/consumers.py
@@ -29,3 +29,34 @@ class OrderConsumer(AsyncWebsocketConsumer):
             'message': event['message'],
             'order_id': event['order_id']
         }))
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.channel_layer = get_channel_layer()
+
+    async def connect(self):
+        user = self.scope['user']
+        if user.is_authenticated and (user.is_staff or user.is_superuser):
+            await self.channel_layer.group_add(
+                "notificaciones_admin",
+                self.channel_name
+            )
+            await self.accept()
+        else:
+            await self.close()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            "notificaciones_admin",
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        pass
+
+    async def notification_sent(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'notification',
+            'message': event['message']
+        }))
