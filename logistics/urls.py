629c47f71ff83b17f0271933769a27d40a7f2507
jabarmagdiel <jabarmagdiel@gmail.com>
2025-11-11 07:53:35 -0400

    Add InventoryMovementViewSet to URLs

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import AlertViewSet, RecommendationViewSet
+from .views import AlertViewSet, RecommendationViewSet, InventoryMovementViewSet
 
 router = DefaultRouter()
 router.register(r'alerts', AlertViewSet)
 router.register(r'recommendations', RecommendationViewSet)
-
+router.register(r'inventory', InventoryMovementViewSet, basename='inventory')
 urlpatterns = router.urls
